b77e7452ebd6e789cd65f5a49076380273baca63
Magnus Søndergaard Christensen <magn0811@edu.campusvejle.dk>
2017-06-01 10:26:26 +0200

    folders upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/style.css"> </head>
+
+<body>
+    <div class="container">
+        <h1>hello</h1></div>
+</body>
+
+</html>
