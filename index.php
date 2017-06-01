25a6d3ce24ca40068550ca23cd0efdc0d3ce00fa
Magnus Søndergaard Christensen <magn0811@edu.campusvejle.dk>
2017-06-01 14:15:43 +0200

    done?

--- index.php
+++ index.php
@@ -10,40 +10,71 @@
 
 <body>
     <div class="container">
+        <!-- Header -->
         <header>
             <div class="row">
                 <div class="twelev columns banner">
                     <h1>Nap all day <br> Sleep all night <br> Party never.</h1> </div>
             </div>
         </header>
+        <!-- Header end -->
         <div class="clear"></div>
         <div class="row">
+            <!-- Snail icon -->
             <div class="icon">
-                <div class="one columns"><img src="img/Animals-Snail-icon-32.png" alt=""> </div>
+                <div class="one columns">
+                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank"><img src="img/Animals-Snail-icon-32.png" alt="Snail logo"></a>
+                </div>
             </div>
+            <!-- Navigation -->
             <div class="eleven columns">
                 <nav>
                     <ul>
+                        <li><a href="#">Forside</a></li>
                         <li><a href="#">EPIC</a></li>
-                        <li><a href="#">AWESOME</a></li>
-                        <li><a href="#">LEGENDARY</a></li>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">LEGENDARY </a></li>
                     </ul>
                 </nav>
             </div>
         </div>
         <div class="row">
+            <!-- Main -->
             <div class="eight columns">
                 <main> <img src="img/sloth1Edit.jpg" alt="">
-                    <p>Dolla dolla bill y'all.</p>
+                    <h2>Dolla dolla bill y'all.</h2>
+                    <!-- Dato -->
+                    <div class="date">
+                        <p id="demo"></p>
+                    </div>
+                    <!-- Dato end -->
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ratione placeat, natus delectus unde amet, inventore temporibus tempora maxime autem accusamus aperiam ducimus, reprehenderit, omnis culpa quod harum reiciendis dolore!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos aliquam rerum impedit temporibus officia consectetur, nisi, nihil officiis perferendis veritatis dolorum vel eaque, praesentium qui dolores provident alias sapiente eveniet!</p>
+                    <h3>Lorem Ipsum</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit tenetur nulla beatae, possimus in repellat placeat doloremque ipsum rem sit! Quibusdam at iste pariatur voluptas eligendi excepturi necessitatibus nisi quas.</p>
+                    <hr> <img src="img/sloth5Edit.jpg" alt="">
+                    <h2>Idk hvad det her er? &uArr;</h2>
+                    <!-- Dato -->
+                    <div class="date">
+                        <p>Dolla dolla bill y'all</p>
+                    </div>
+                    <!-- Dato end -->
                 </main>
-            </div>
+                <!-- Main end -->
+                <!-- Footer -->
+                <footer> Footer: info info info info info info </footer>
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+                    <button>Dont you dare press this!</button>
+                </a>
+                <hr> </div>
+            <!-- Aside -->
             <div class="four columns">
                 <aside>
                     <div class="box">
                         <div class="headBox">
                             <p>Log ind</p>
                         </div>
-                        <div class="infoBox">
+                        <div class="loginBox">
                             <label for="frame">
                                 <div class="textFrame">Brugernavn:</div>
                             </label>
@@ -55,10 +86,23 @@
                             <button>Log ind</button>
                         </div>
                     </div>
+                    <h3>EPIC LEGENDARY SLOTHS</h3>
+                    <div class="box">
+                        <div class="headBox">
+                            <p>What is this?!?!</p>
+                        </div>
+                        <div class="infoBox">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates sunt fuga laborum consectetur fugit recusandae minima asperiores eveniet dolore impedit quod earum ea debitis inventore, quas corrupti, molestias ipsam similique?</p>
+                        </div>
+                    </div>
                 </aside>
             </div>
+            <!-- Aside end -->
         </div>
     </div>
+    <script>
+        document.getElementById("demo").innerHTML = Date();
+    </script>
 </body>
 
 </html>
