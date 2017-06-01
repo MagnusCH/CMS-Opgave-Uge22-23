5e358abda8682a0ea514923665fb143fac55b5ca
Magnus Søndergaard Christensen <magn0811@edu.campusvejle.dk>
2017-06-01 12:42:45 +0200

    update

--- index.php
+++ index.php
@@ -10,7 +10,55 @@
 
 <body>
     <div class="container">
-        <h1>hello</h1></div>
+        <header>
+            <div class="row">
+                <div class="twelev columns banner">
+                    <h1>Nap all day <br> Sleep all night <br> Party never.</h1> </div>
+            </div>
+        </header>
+        <div class="clear"></div>
+        <div class="row">
+            <div class="icon">
+                <div class="one columns"><img src="img/Animals-Snail-icon-32.png" alt=""> </div>
+            </div>
+            <div class="eleven columns">
+                <nav>
+                    <ul>
+                        <li><a href="#">EPIC</a></li>
+                        <li><a href="#">AWESOME</a></li>
+                        <li><a href="#">LEGENDARY</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="row">
+            <div class="eight columns">
+                <main> <img src="img/sloth1Edit.jpg" alt="">
+                    <p>Dolla dolla bill y'all.</p>
+                </main>
+            </div>
+            <div class="four columns">
+                <aside>
+                    <div class="box">
+                        <div class="headBox">
+                            <p>Log ind</p>
+                        </div>
+                        <div class="infoBox">
+                            <label for="frame">
+                                <div class="textFrame">Brugernavn:</div>
+                            </label>
+                            <input type="text" id="brugernavn" name="brugernavn">
+                            <label for="frame">
+                                <div class="textFrame">Password:</div>
+                            </label>
+                            <input type="text" id="password" name="password">
+                            <button>Log ind</button>
+                        </div>
+                    </div>
+                </aside>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
